e08f2af993d60a5b21a968e6418f6ee750be0bf3
mahdiyar <mahdiyar472@gmail.com>
2023-05-28 05:43:16 +0330

    add useReducerAsync package and configure AuhContext for sign in

--- Context/AuthContext.js
+++ Context/AuthContext.js
@@ -1,22 +1,76 @@
-'use client'
+"use client";
+import axios from "axios";
 import { createContext, useContext, useReducer } from "react";
+import { toast } from "react-hot-toast";
+import { useReducerAsync } from "use-reducer-async";
 
 const AuthContext = createContext();
 const AuthContextDispatcher = createContext();
 
 const initialState = {
   user: null,
-  loading: true,
+  loading: false,
   error: null,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case "SIGNIN-PENDING": {
+      return {
+        user: null,
+        loading: true,
+        error: null,
+      };
+    }
+    case "SIGNIN-SUCCESS": {
+      return {
+        user: action.payload,
+        loading: false,
+        error: null,
+      };
+    }
+    case "SIGNIN-FAILURE": {
+      return {
+        user: null,
+        loading: false,
+        error: action.error,
+      };
+    }
+    default: {
+      return state;
+    }
   }
 };
 
+const asyncActionHandlers = {
+  SIGNIN:
+    ({ dispatch }) =>
+    async (action) => {
+      dispatch({ type: "SIGNIN-PENDING" });
+      await axios
+        .post("http://localhost:5000/api/user/signin", action.payload, {
+          withCredentials: true, //must be set for receive http only cookies form backend
+        })
+        .then((res) => {
+          toast.success("خوش آمدید");
+          dispatch({ type: "SIGNIN-SUCCESS", payload: res.data });
+        })
+        .catch((err) => {
+          toast.error(err.response?.data?.message);
+          dispatch({
+            type: "SIGNIN-FAILURE",
+            error: err.response?.data?.message,
+          });
+        });
+    },
+};
+
 const AuthProvider = ({ children }) => {
-  const [user, dispatch] = useReducer(reducer, initialState);
+  const [user, dispatch] = useReducerAsync(
+    reducer,
+    initialState,
+    asyncActionHandlers
+  );
   return (
     <AuthContext.Provider value={user}>
       <AuthContextDispatcher.Provider value={dispatch}>
--- app/signin-signup/Signin/Signin.jsx
+++ app/signin-signup/Signin/Signin.jsx
@@ -1,32 +1,30 @@
-'use client'
+"use client";
 import axios from "axios";
 import { useFormik } from "formik";
 import { toast } from "react-hot-toast";
 import * as Yup from "yup";
 import { useRouter } from "next/navigation";
+import { useAuthActions, useAuth } from "@/Context/AuthContext";
 
 const initialValues = {
   email: "",
   password: "",
 };
+
 const validationSchema = Yup.object({
   email: Yup.string().required("ایمیل را وارد کنید").email("ایمیل نامعتبر است"),
   password: Yup.string()
     .required("رمز عبور را وارد کنید")
     .min(8, "رمز عبور کوتاه است"),
 });
+
 const Signin = () => {
   const router = useRouter();
+  const dispatch = useAuthActions();
+  const { loading } = useAuth();
+  console.log("🚀 ~ file: Signin.jsx:25 ~ Signin ~ loading:", loading);
   const onSubmit = (values) => {
-    axios
-      .post("http://localhost:5000/api/user/signin", values, {
-        withCredentials: true, //must be set for receive http only cookies form backend
-      })
-      .then(() => {
-        toast.success("خوش آمدید")
-        router.push("/"); 
-      })
-      .catch((err) => toast.error(err.response.data.message));
+    dispatch({ type: "SIGNIN", payload: values });
   };
 
   const formik = useFormik({
@@ -78,10 +76,29 @@ const Signin = () => {
         />
       </div>
       <button
-        className="bg-blue-500 rounded-lg px-10 py-2 w-fit text-white"
+        className={` rounded-lg px-10 py-2 w-fit text-white flex justify-center ${loading?"bg-blue-300":"bg-blue-500"}`}
         type="submit"
+        disabled={loading}
       >
-        ورود
+        {loading && (
+          <svg
+            aria-hidden="true"
+            class="inline w-6 h-6 text-white animate-spin fill-blue-500"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+        )}
+       {!loading && <span>ورود</span>}
       </button>
     </form>
   );
